f6d9720e931cea213c7617f4667fbc962c4f3396
beldenge <beldenge@gmail.com>
2011-12-13 22:15:30 -0500

    Committing complete project to repository.

---
+++ src/test/java/com/ciphertool/cfgreader/util/ContextFreeGrammarHelperTest.java
@@ -0,0 +1,29 @@
+package com.ciphertool.cfgreader.util;
+import java.io.File;
+
+import javax.xml.bind.JAXBException;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ciphertool.cfgreader.datastructures.Tree;
+import com.ciphertool.cfgreader.generated.ProductionType;
+
+public class ContextFreeGrammarHelperTest {
+	
+	static Logger log = Logger.getLogger(ContextFreeGrammarHelperTest.class);
+	ContextFreeGrammarHelper cfgHelper;
+	
+	@Before
+	public void setUp() throws Exception {
+		File grammarFile = new File("src\\data\\xml\\EnglishSentenceCFG.xml");
+		cfgHelper = new ContextFreeGrammarHelper(grammarFile);
+	}
+	
+	@Test
+	public void testGenerateRandomSyntaxTree() throws JAXBException {
+		Tree<ProductionType> syntaxTree = cfgHelper.generateRandomSyntaxTree();
+		log.info(syntaxTree.toString());
+	}
+}
