c9c3b9d1736c7b861c57ffdeec4df0281bd56f1b
george <george@10.0.0.4>
2012-10-21 23:57:16 -0400

    Added the GPL.

--- src/main/java/com/ciphertool/cfgreader/generated/ExpansionType.java
+++ src/main/java/com/ciphertool/cfgreader/generated/ExpansionType.java
@@ -5,10 +5,29 @@
 // Generated on: 2011.09.20 at 08:01:46 AM EDT 
 //
 
+/**
+ * Copyright 2012 George Belden
+ * 
+ * This file is part of CFGReader.
+ * 
+ * CFGReader is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * CFGReader is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * CFGReader. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.ciphertool.cfgreader.generated;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
